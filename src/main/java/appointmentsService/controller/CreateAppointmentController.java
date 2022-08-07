cc2904ff352185218d89e1ca96a3e2eb56b5bb8c
henriktimpel <73823265+henriktimpel@users.noreply.github.com>
2022-08-07 14:34:29 +0200

    Rest api Create Controller
    Create the rest of the Create Controllers

--- src/main/java/appointmentsService/controller/CreateAppointmentController.java
+++ src/main/java/appointmentsService/controller/CreateAppointmentController.java
@@ -3,25 +3,23 @@ package appointmentsService.controller;
 
 import appointmentsService.model.Appointment;
 import appointmentsService.repository.AppointmentRepository;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/appointment")
-public class AppointmentController {
+@RequestMapping("/createAppointments")
+public class CreateAppointmentController {
+
 
     private AppointmentRepository appointmentRepository;
 
-    public AppointmentController(AppointmentRepository appointmentRepository) {
+    public CreateAppointmentController(AppointmentRepository appointmentRepository) {
         this.appointmentRepository = appointmentRepository;
     }
 
-    @GetMapping("")
-    public List<Appointment> appointmentIndex() {
-        return appointmentRepository.findAll();
+    @PostMapping("")
+    public Appointment createAppointment(Appointment appointment) {
+    return appointmentRepository.save(appointment);
     }
 }
-
--- src/main/java/appointmentsService/repository/AppointmentRepository.java
+++ src/main/java/appointmentsService/repository/AppointmentRepository.java
@@ -3,6 +3,6 @@ package appointmentsService.repository;
 import appointmentsService.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AppointmentRepository /**extends JpaRepository<Appointment, Integer> **/ {
+public interface AppointmentRepository extends JpaRepository<Appointment, Integer>  {
 
 }
