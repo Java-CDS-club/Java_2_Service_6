5910d04f74b389faf7575951b3fbc639333c11e2
DEVmozi <zid.m@pm.me>
2022-08-07 14:43:40 +0200

    added "searchAppointments" method

--- src/main/java/appointmentsService/sessionControls/s_Appointment.java
+++ src/main/java/appointmentsService/sessionControls/s_Appointment.java
@@ -115,4 +115,50 @@ public class s_Appointment extends sessionParentClass {
             }
         }
     }
+    /** Search for a public appointments by certain criteria
+     * @param searchTerm search term included in the title of the appointment
+     * @param fromDate define the start of time interval, can only be used in conjunction with "toDate" (see below)
+     * @param toDate define the end of time interval, can only be used in conjunction with "fromDate"
+     * @param courseID show appointments from one course
+     * @param facultyID show appointments from one faculty
+     * @param locationID show appointments from one location
+     * @param personId show appointments from one organizer
+     * @return list of type <Appointment> containing matching appointments
+     */
+    public List<Appointment> searchAppointments(String searchTerm, Integer semester, Timestamp fromDate, Timestamp toDate, BigInteger courseID,
+                                                BigInteger facultyID, BigInteger locationID, BigInteger personId) {
+        getSessionFactory();
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Appointment> cr = criteriaBuilder.createQuery(Appointment.class);
+            Root<Appointment> root = cr.from(Appointment.class);
+            ArrayList<Predicate> predicates = new ArrayList<Predicate>();
+
+            predicates.add(criteriaBuilder.isTrue(root.get("ISPUBLIC")));
+            if (searchTerm != null) {
+                predicates.add(criteriaBuilder.like(root.get("TITLE"), "%" + searchTerm + "%"));
+            }
+            if (fromDate != null  && toDate != null) {
+                predicates.add(criteriaBuilder.or(criteriaBuilder.lessThan(root.get("STARTDATETIME"), toDate), criteriaBuilder.greaterThan(root.get("ENDDATETIME"), fromDate )));
+            }
+            if (semester != null) {
+                predicates.add(criteriaBuilder.equal(root.get("SEMESTER"), semester));
+            }
+            if (courseID != null) {
+                predicates.add(criteriaBuilder.equal(root.get("COURSE_ID"), courseID));
+            }
+            if (facultyID != null) {
+                predicates.add(criteriaBuilder.equal(root.get("FACULTY_ID"), facultyID));
+            }
+            if (locationID != null) {
+                predicates.add(criteriaBuilder.equal(root.get("LOCATION_ID"), locationID));
+            }
+            if (personId != null) {
+                predicates.add(criteriaBuilder.equal(root.get("PERSON_ID"), personId));
+            }
+            Predicate[] array = predicates.toArray(new Predicate[0]);
+            cr.select(root).where(array);
+            return session.createQuery(cr).list();
+
+        }
 }
